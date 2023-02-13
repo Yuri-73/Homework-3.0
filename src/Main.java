59f162bcd190925fdd7f367a5b46af841efe74bc
Юрий <fedejkin@gmail.com>
2023-02-13 20:17:20 +0300

    task-1 + task-2 + task-3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
-
     public static void task1() {
         System.out.println("Задача 1");
         int age1 = 22;
@@ -15,7 +15,6 @@ public class Main {
             System.out.println("Если возраст человека равен " + age2 + " годам, управлять транспортным средством рано");
         }
     }
-
     public static void task2() {
         System.out.println("Задача 2");
         int AirTemperature1 = 4;
@@ -25,6 +24,17 @@ public class Main {
         }
         if (AirTemperature2 > 5) {
             System.out.println("На улице " + AirTemperature2 + " град. - можно идти без шапки");
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int machineSpeed1 = 80;
+        int machineSpeed2 = 55;
+        if (machineSpeed1 >= 60) {
+            System.out.println("Если скорость " + machineSpeed1 + " км/ч, то придётся заплатить штраф");
+        }
+        if (machineSpeed2 < 60) {
+            System.out.println("Если скорость " + machineSpeed2 + " км/ч, можно ездить спокойно");
         }
     }
 }

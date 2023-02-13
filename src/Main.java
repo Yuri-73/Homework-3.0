710c4fa5ee2b350c37744bc3f14528677b29613e
Юрий <fedejkin@gmail.com>
2023-02-13 19:54:34 +0300

    task-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int age1 = 22;
+        int age2 = 17;
+        if(age1 >= 18) {
+           System.out.println("Если возраст человека равен " + age1 + " годам, то он достиг совершенолетия и может управлять транспортным средством");
+        }
+        if(age2 < 18) {
+            System.out.println("Если возраст человека равен " + age2 + " годам, управлять транспортным средством рано");
+        }
     }
 }
+
+
